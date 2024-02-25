53a4faaa943bf70058aadc4497e73b0513bcf291
Leameesook <leamspenven@gmail.com>
2024-02-25 15:32:02 +0100

    Save

---
+++ Gamelle/Core/Src/sensor_ir.c
@@ -0,0 +1,15 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : sensor_ir.c
+  * @author			: Léa Penven
+  *	@date			: 25/02/2024
+  * @brief          : Capteur IR
+  *
+  ******************************************************************************
+  */
+
+#include "sensor_ir.h"
+#include "usart.h"
+#include "i2c.h"
+
